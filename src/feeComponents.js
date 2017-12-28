3fdc3ee1fe7dd93a59501a55ef4a1652213c8af6
horyd <prods3@gmail.com>
2017-12-28 12:04:31 +1100

    Correct export of the fee component calculations

--- src/feeComponents.js
+++ src/feeComponents.js
@@ -8,40 +8,41 @@ function capitalisedFutureDisposalFee(fee, term, rate) {
   return (fee * rate / 12) * (1 / (((1 + rate / 12) ** term) - 1)) * (1 / (1 + rate / 12));
 }
 
-export function disposalSchedule(fee, term, rate) {
-  const ratePlusOne = 1 + rate / 12;
-  const payment = capitalisedFutureDisposalFee(fee, term, rate);
-  const accumulation = Array.from({ length: term }, () => payment).reduce((amort, pmt, i) => {
-    if (i === 0) {
-      amort.push(pmt);
-    } else {
-      amort.push(_.last(amort) * ratePlusOne + pmt);
-    }
-    return amort;
-  }, []);
-  const balances = accumulation.map((a, i) => a - payment);
+export default {
+  disposalSchedule: function disposalSchedule(fee, term, rate) {
+    const ratePlusOne = 1 + rate / 12;
+    const payment = capitalisedFutureDisposalFee(fee, term, rate);
+    const accumulation = Array.from({ length: term }, () => payment).reduce((amort, pmt, i) => {
+      if (i === 0) {
+        amort.push(pmt);
+      } else {
+        amort.push(_.last(amort) * ratePlusOne + pmt);
+      }
+      return amort;
+    }, []);
+    const balances = accumulation.map((a, i) => a - payment);
 
-  return {
-    balances,
-    payment,
-  };
-}
+    return {
+      balances,
+      payment,
+    };
+  },
+  originationSchedule: function originationSchedule(fee, term, rate) {
+    const ratePlusOne = 1 + rate / 12;
+    const payment = capitalisedOriginationFee(fee, term, rate);
+    const amortisation = Array.from({ length: term }, () => payment).reduce((amort, pmt, i) => {
+      if (i === 0) {
+        amort.push(fee - pmt);
+      } else {
+        amort.push(_.last(amort) * ratePlusOne - pmt);
+      }
+      return amort;
+    }, []);
+    const balances = amortisation.map((a, i) => a + payment);
 
-export function originationSchedule(fee, term, rate) {
-  const ratePlusOne = 1 + rate / 12;
-  const payment = capitalisedOriginationFee(fee, term, rate);
-  const amortisation = Array.from({ length: term }, () => payment).reduce((amort, pmt, i) => {
-    if (i === 0) {
-      amort.push(fee - pmt);
-    } else {
-      amort.push(_.last(amort) * ratePlusOne - pmt);
-    }
-    return amort;
-  }, []);
-  const balances = amortisation.map((a, i) => a + payment);
-
-  return {
-    balances,
-    payment,
-  };
-}
+    return {
+      balances,
+      payment,
+    };
+  },
+};

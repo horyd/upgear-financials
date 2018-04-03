6332b0aa23e43662eb03d453c463eb332c0d9ea9
horyd <prods3@gmail.com>
2018-04-03 22:53:39 +1000

    fixed test and build

--- test/feeComponents.spec.js
+++ test/feeComponents.spec.js
@@ -1,6 +1,5 @@
 import { originationSchedule, disposalSchedule } from '../src/feeComponents';
 import { expect } from 'chai';
-import _ from 'lodash';
 
 describe('Fee Schedules', function () {
   it('computes an origination schedule', function (done) {
@@ -10,10 +9,10 @@ describe('Fee Schedules', function () {
     const originations = originationSchedule(fee, term, rate);
 
     expect(
-      _.last(originations.balances) * (1 + rate / 12) - originations.payment
+      originations.balances[originations.balances.length - 2] * (1 + rate / 12) - originations.payment
     ).to.be.closeTo(0, fee / 3000, 'Origination fee did not amortise correctly.');
     expect(originations.balances[0]).to.equal(fee);
-    expect(originations.balances.length).to.equal(term);
+    expect(originations.balances.length).to.equal(term + 1);
     done();
   });
   it('computes a disposal schedule', function (done) {
@@ -23,10 +22,10 @@ describe('Fee Schedules', function () {
     const disposals = disposalSchedule(fee, term, rate);
 
     expect(
-      _.last(disposals.balances) * (1 + rate / 12) + disposals.payment
+      disposals.balances[disposals.balances.length - 2] * (1 + rate / 12) + disposals.payment
     ).to.be.closeTo(fee, fee / 3000, 'Disposal balance did not add to fee.');
     expect(disposals.balances[0]).to.equal(0);
-    expect(disposals.balances.length).to.equal(term);
+    expect(disposals.balances.length).to.equal(term + 1);
     done();
   });
 });

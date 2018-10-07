f2bb5c27ea370abab3b5d1426a0dd13ca3decaf4
horyd <prods3@gmail.com>
2018-10-08 10:46:27 +1100

    Changes to age factor in depreciation

--- src/depreciation.js
+++ src/depreciation.js
@@ -7,27 +7,27 @@ export default (rrp, rv, depreciationPower, operatingLife, writeOffMonths, age =
   // const k = 3 * ((rrp - rv) - (rv / writeOffMonths * (operatingLife - age))) / (operatingLife - age)**3;
   // n-ic
   const k = depreciationPower * (
-    (rrp - rv) - (depnInWriteOffPeriod * (operatingLife - age))
-  ) / (operatingLife - age) ** depreciationPower;
+    (rrp - rv) - (depnInWriteOffPeriod * (operatingLife))
+  ) / (operatingLife) ** depreciationPower;
 
   const byMonth = Array.from({
-    length: (operatingLife - age + writeOffMonths + 1),
+    length: (operatingLife + writeOffMonths + 1),
   }, (v, i) => {
     if (i === 0) return rrp;
-    if (i <= (operatingLife - age)) {
+    if (i <= operatingLife) {
       // linear
       // const y = k/2 * ((operatingLife - age)**2 - (i - (operatingLife - age))**2) + depnInWriteOffPeriod * i;
       // parabolic
       // const y = k/3 * ((operatingLife - age)**3 + (i - (operatingLife - age))**3) + depnInWriteOffPeriod * i;
       // n-ic
       const y = (k / depreciationPower) * (
-        (operatingLife - age) ** depreciationPower - (i - (operatingLife - age)) ** depreciationPower
+        (operatingLife) ** depreciationPower - (i - (operatingLife)) ** depreciationPower
       ) + depnInWriteOffPeriod * i;
 
       return rrp - y;
     }
-    return rv - (depnInWriteOffPeriod * (i - (operatingLife - age)));
+    return rv - (depnInWriteOffPeriod * (i - operatingLife));
   });
 
-  return byMonth;
+  return byMonth.slice(age);
 };
--- test/depreciation.spec.js
+++ test/depreciation.spec.js
@@ -3,16 +3,30 @@ import { expect } from 'chai';
 
 describe('Depreciation Schedule', function () {
   it('computes a basic depreciation', function (done) {
-    const rrp = 3000;
-    const rv = 600;
-    const operatingLife = 48;
-    const writeOffMonths = 36;
-    const depreciationPower = 4;
+    const rrp = 10000;
+    const rv = 2000;
+    const operatingLife = 36;
+    const writeOffMonths = 24;
+    const depreciationPower = 2;
     const basicDepn = depreciationSchedule(rrp, rv, depreciationPower, operatingLife, writeOffMonths);
-
+    console.log(basicDepn);
     expect(basicDepn[0]).to.equal(rrp);
     expect(basicDepn[operatingLife]).to.equal(rv);
     expect(basicDepn[operatingLife + writeOffMonths]).to.equal(0);
+    done();
+  });
+
+  it('computes a depreciation schedule for an aged asset', function (done) {
+    const rrp = 10000;
+    const rv = 2000;
+    const operatingLife = 36;
+    const writeOffMonths = 24;
+    const depreciationPower = 2;
+    const age = 12;
+    const basicDepn = depreciationSchedule(rrp, rv, depreciationPower, operatingLife, writeOffMonths, age);
+    console.log(basicDepn);
+    expect(basicDepn[operatingLife - age]).to.equal(rv);
+    expect(basicDepn[operatingLife + writeOffMonths - age]).to.equal(0);
     done();
   });
 });
